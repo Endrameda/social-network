3bae292de888e807601366ccd4aa6d2b4a1125bf
bead <endrameda98@gmail.com>
2020-11-19 16:18:49 +0500

    Styling component and refactoring to class component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,47 +3,47 @@ import styles from './users.module.scss';
 import * as axios from "axios";
 import userPhoto from '../../assets/img/icons/User avatar.svg'
 
-const Users = (props) => {
-	const getUsers = () => {
+export default class Users extends React.Component {
+	constructor(props) {
+		super(props);
 		if (props.users.length === 0) {
 			axios.get('https://social-network.samuraijs.com/api/1.0/users')
 				.then(response => {
-					props.setUsers(response.data.items);
+					this.props.setUsers(response.data.items);
 				});
 		}
 	}
 
-	return <div className={styles.usersList}>
-		<button onClick={getUsers}>Get users</button>
-		{
-			props.users.map(user => <div key={user.id} className={styles.item}>
-				<div className={styles.itemLeft}>
-					<div className={styles.imageWrapper}>
-						<img src={
-							user.photos.small !== null
-								? user.photos.small
-								: userPhoto} alt={user.name}/>
+	render() {
+		return <div className={styles.usersList}>
+			{
+				this.props.users.map(user => <div key={user.id} className={styles.item}>
+					<div className={styles.itemLeft}>
+						<div className={styles.imageWrapper}>
+							<img src={
+								user.photos.small !== null
+									? user.photos.small
+									: userPhoto} alt={user.name}/>
+						</div>
+						{user.followed
+							? <button onClick={() => this.props.unfollow(user.id)}>Unfollow</button>
+							: <button onClick={() => this.props.follow(user.id)}>Follow</button>}
 					</div>
-					{user.followed
-						? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
-						: <button onClick={() => props.follow(user.id)}>Follow</button>}
-				</div>
-				<div className={styles.itemRIght}>
-					<div className={styles.userInfo}>
-						<span className={styles.fullName}>{user.name}</span>
-						{user.status !== null
-							? <span className={styles.status}>{user.status}</span>
-							: ''}
+					<div className={styles.itemRight}>
+						<div className={styles.userInfo}>
+							<span className={styles.fullName}>{user.name}</span>
+							{user.status !== null
+								? <span className={styles.status}>{user.status}</span>
+								: ''}
 
+						</div>
+						<div className={styles.location}>
+							<span className={styles.locationCountry}>Страна: {"user.location.country"}</span>
+							<span className={styles.locationCity}>Город: {"user.location.city"}</span>
+						</div>
 					</div>
-					<div className={styles.location}>
-						<span className={styles.locationCountry}>{"user.location.country"}</span>
-						<span className={styles.locationCity}>{"user.location.city"}</span>
-					</div>
-				</div>
-			</div>)
-		}
-	</div>
+				</div>)
+			}
+		</div>
+	}
 }
-
-export default Users;
